5a4b5aa01df4dc639e619adc6434a9cb529c29c3
Abhishek Srivastava <abhsrivastava@researchnow.com>
2018-04-29 13:35:43 -0500

    use scss in tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
+require ('../styles/style.scss')
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-
 import {Login} from './login'
 
 ReactDOM.render(<Login />, document.getElementById('root'))
