6d2c23361430b79338455e3563c2c278358d1127
Abhishek Srivastava <abhsrivastava@researchnow.com>
2018-04-29 13:27:42 -0500

    initial implementation

---
+++ src/login.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+
+export class Login extends React.Component<LoginProps, LoginState>{
+  constructor(props: LoginProps) {
+    super(props)
+  }
+  render() { return (
+    <form className="col-md-4">
+      <div className="form-group">
+        <label htmlFor="inputEmail">Email</label>
+        <input type="email" className="form-control" id="inputEmail" placeholder="Email"/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="inputPassword">Password</label>
+        <input type="password" className="form-control" id="inputPassword" placeholder="Password"/>
+      </div>
+      <div className="checkbox form-group">
+        <label>
+          <input id="rem" type="checkbox" />Remember Me
+        </label>
+      </div>
+      <button type="submit" className="btn btn-default">Submit</button>
+    </form>
+  )}
+}
+
+interface LoginProps {
+
+}
+
+interface LoginState {
+
+}
